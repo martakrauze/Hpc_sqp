942c7c3479015c67514bf9789adec0ae4125207f
Marta Krauze <tygrysmws@gmail.com>
2023-05-23 21:53:11 +0200

    First working version

---
+++ slower/build/CMakeFiles/bench_demo.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/marta/Hpc_sqp/slower/src/traj_opt.cpp" "CMakeFiles/bench_demo.dir/src/traj_opt.cpp.o" "gcc" "CMakeFiles/bench_demo.dir/src/traj_opt.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
